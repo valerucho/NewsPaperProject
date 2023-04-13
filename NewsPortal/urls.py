907770b949ec231ceb3f0ee0a46d58692a1ac8d7
valerucho <114688800+valerucho@users.noreply.github.com>
2023-04-13 19:00:19 +0300

    6163

--- NewsPortal/urls.py
+++ NewsPortal/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('pages/', include('django.contrib.flatpages.urls')),
-   # Делаем так, чтобы все адреса из нашего приложения (news/urls.py)
-   # подключались к главному приложению с префиксом news/.
+   path('accounts/', include('django.contrib.auth.urls')),
    path('news/', include('news.urls')),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,5 @@
-# Импортируем класс, который говорит нам о том,
-# что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from .filters import PostFilter
 from .forms import PostForm
@@ -8,81 +7,50 @@ from django.urls import reverse_lazy
 
 
 class NewsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Post
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = '-add_date'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
-    paginate_by = 10  # вот так мы можем указать количество записей на странице
+    paginate_by = 10
 
 
 class PostDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Post
-    # Используем другой шаблон — product.html
     template_name = 'post.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'post'
 
 
 class NewsSearch(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Post
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = '-add_date'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'news_search.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news_search'
-    paginate_by = 10  # вот так мы можем указать количество записей на странице
+    paginate_by = 10
 
     def get_queryset(self):
-        # Получаем обычный запрос
         queryset = super().get_queryset()
-        # Используем наш класс фильтрации.
-        # self.request.GET содержит объект QueryDict
-        # Сохраняем нашу фильтрацию в объекте класса,
-        # чтобы потом добавить в контекст и использовать в шаблоне.
         self.filterset = PostFilter(self.request.GET, queryset)
-        # Возвращаем из функции отфильтрованный список товаров
         return self.filterset.qs
 
     def get_context_data(self, **kwargs):
-        # С помощью super() мы обращаемся к родительским классам
-        # и вызываем у них метод get_context_data с теми же аргументами,
-        # что и были переданы нам.
-        # В ответе мы должны получить словарь.
         context = super().get_context_data(**kwargs)
-        # Добавляем в контекст объект фильтрации.
         context['filterset'] = self.filterset
         return context
 
 
-# Добавляем новое представление для публикации новости.
-class PostCreate(CreateView):
-    # Указываем нашу разработанную форму
+class PostCreate(LoginRequiredMixin, CreateView):
+    raise_exeption = True
     form_class = PostForm
-    # модель новостей
     model = Post
-    # и новый шаблон, в котором используется форма.
     template_name = 'post_edit.html'
 
 
-# Добавляем представление для изменения новости/статьи.
 class PostUpdate(UpdateView):
     form_class = PostForm
     model = Post
     template_name = 'post_edit.html'
 
 
-# Представление, удаляющее новость/статью.
 class PostDelete(DeleteView):
     model = Post
     template_name = 'post_delete.html'
